33d36b8f91f51ba7377ecee877bdac94f68ea815
dinhoSilwa <dinhosilwa_@hotmail.com>
2024-07-31 11:07:40 -0300

    FIX : remove default config of mongoose

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,7 @@
-
-//#2 create express serv
-import express  from "express";
+import express from "express";
 import mongoose from "mongoose";
 import dotenv from 'dotenv';
-
+import router from "./routes/createadmroute";
 
 dotenv.config();
 
@@ -11,14 +9,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use('create/admusers', admRoutes);
+app.use('/create/admusers', router); // Corrigido para incluir a barra inicial
 
-mongoose.connect(process.env.MONGO_URI as string, {
-  useNewUrlParse : true, 
-  useUnifieldTopology : true
-}).then(()=> {
-  console.log(`Conectado com Sucesso ao Banco de Dados`);
-  app.listen(PORT, () => console.log(`Rodando na porta ${PORT}`))
+mongoose.connect(process.env.MONGO_URI as string).then(() => {
+  console.log(`Conectado com sucesso ao banco de dados`);
+  app.listen(PORT, () => console.log(`Rodando na porta ${PORT}`));
 }).catch(error => {
-  console.error("Falha ao conectar", error)
-})
+  console.error("Falha ao conectar", error);
+});
