ede2095fc77f8d7a3696e5f8c8a93896e2855b2d
dinhoSilwa <dinhosilwa_@hotmail.com>
2024-07-31 10:49:18 -0300

    FEAT : (model) add adm model

---
+++ backend/src/models/adminuser.ts
@@ -0,0 +1,25 @@
+import { Schema, model } from "mongoose";
+
+interface AdmUserProps {
+  name: string;
+  email: string;
+  password: string;
+  role: string,
+  avatar: string;
+  createdAt: Date;
+  updateAt: Date;
+
+}
+const admSchema = new Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { type: String, required: true },
+  avatar: { type: String, required: true }
+}, {
+  timestamps: true
+})
+
+const admUser = model<AdmUserProps>('adm-user', admSchema)
+
+export default admUser
