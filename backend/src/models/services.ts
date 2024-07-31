44b9fd9216355b7bb3c2b1129d16026d1df718d1
dinhoSilwa <dinhosilwa_@hotmail.com>
2024-07-31 10:25:51 -0300

    FEAT : create service models

--- backend/src/models/services.ts
+++ backend/src/models/services.ts
@@ -1,5 +1,29 @@
-import {Schema} from "mongoose";
+import { model, Schema, Document } from "mongoose";
 
-export const useService = new Schema({
-  serviceName : {type : String}
-})
+interface ServiceProps extends Document {
+  serviceName: string;
+  servicePrice: string;
+  serviceDetails: string;
+  serviceType: string;
+  serviceDuration: string;
+  serviceowner: string;
+  serviceUrl: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const serviceSchema = new Schema<ServiceProps>({
+  serviceName: { type: String, required: true },
+  servicePrice: { type: String, required: true },
+  serviceDetails: { type: String, required: true },
+  serviceType: { type: String, required: true },
+  serviceDuration: { type: String, required: true },
+  serviceowner: { type: String, required: true },
+  serviceUrl: { type: String, required: true }
+}, {
+  timestamps: true
+});
+
+const serviceModel = model<ServiceProps>('Service', serviceSchema);
+
+export default serviceModel;
