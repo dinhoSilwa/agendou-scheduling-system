c87595b9e69f4fe8523e2cc037c6899c78f2dff8
dinhoSilwa <dinhosilwa_@hotmail.com>
2024-08-07 13:48:33 -0300

    FEAT :  data model for admin users

--- backend/src/models/adminuser.ts
+++ backend/src/models/adminuser.ts
@@ -1,18 +1,16 @@
 import { Schema, model } from "mongoose";
-import type { AdmUserProps } from "../@types/globaltypes";
+import type { AdminUserProps } from "../@types/globaltypes";
 
-const admSchema = new Schema({
+const adminSchema = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role: { type: String, required: true },
-  avatar: { type: String, required: false },
-  storename : {type : String, required : true},
-  storeType : {type : String, required : true},
+  phone : {type : String, required : true}
+
 }, {
   timestamps: true
 })
 
-const AdmUser = model<AdmUserProps>('adm-user', admSchema)
+const AdminUser = model<AdminUserProps>('adm-user', adminSchema)
 
-export default AdmUser
+export default AdminUser
