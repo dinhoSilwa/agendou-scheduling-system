31a0bbf250667a489b95d8f7ebec75fc22dc1057
dinhoSilwa <dinhosilwa_@hotmail.com>
2024-08-07 13:51:03 -0300

    FEAT : adimin and business data implementation

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from 'dotenv';
-import router from "./routes/createadmroute";
+import routerBusiness from "./routes/routerBusiness";
+import routerAdmin from "./routes/routeradmin";
 
 dotenv.config();
 
@@ -9,7 +10,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use('/create', router); 
+app.use('/business', routerBusiness); 
+app.use('/admin', routerAdmin)
 
 mongoose.connect(process.env.MONGO_URI as string).then(() => {
   console.log(`Conectado com sucesso ao banco de dados`);
