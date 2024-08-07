58592f7ad2251d9e6d7c35e582bb527dd75b8657
dinhoSilwa <dinhosilwa_@hotmail.com>
2024-08-07 13:47:40 -0300

    FEAT : admin router controllers

--- backend/src/controllers/admcontrollers.ts
+++ backend/src/controllers/admcontrollers.ts
@@ -1,14 +1,14 @@
 import { Request, Response } from "express";
-import AdmUser from "../models/adminuser";
+import AdminUser from "../models/adminuser";
 import bcrypt from 'bcrypt';
  const saltRounds = 10;
 
-export const createAdmUser = async (req : Request, res : Response) =>{
+export const createAdmin = async (req : Request, res : Response) =>{
   try{
-    const {name, email, password, role, avatar, storename, storeType} = req.body;
+    const {name, email, password} = req.body;
     const hashedPassword = await bcrypt.hash(password, saltRounds)
-    const admuser = new AdmUser({name,email, password : hashedPassword, role, avatar, storeType, storename});
-    await admuser.save();
+    const admuser = new AdminUser({name,email, password : hashedPassword});
+    await admuser.save()
     res.status(201).json(admuser);
   } catch(error : any){
     res.status(400).json({message : error.message})
