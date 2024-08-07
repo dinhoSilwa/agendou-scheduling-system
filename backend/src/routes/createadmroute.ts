799df1b59b02c0b4c99946a437b7dbee4f3523d0
dinhoSilwa <dinhosilwa_@hotmail.com>
2024-08-07 13:49:25 -0300

    FEAT : routes for creating administrator profiles

--- backend/src/routes/createadmroute.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { createAdmUser } from "../controllers/admcontrollers";
-
-const router = Router();
-router.post('/', createAdmUser)
-
-export default router
