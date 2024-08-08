d74d62afd501dbd6f8acda5450848b554d7e9ab0
dinhoSilwa <dinhosilwa_@hotmail.com>
2024-08-08 09:21:10 -0300

    FIX : I modified the adiminuser property

--- backend/src/controllers/admcontrollers.ts
+++ backend/src/controllers/admcontrollers.ts
@@ -5,13 +5,15 @@ import bcrypt from 'bcrypt';
 
 export const createAdmin = async (req : Request, res : Response) =>{
   try{
-    const {name, email, password, phone} = req.body;
+    const {adminname, email, password, phone} = req.body;
     const hashedPassword = await bcrypt.hash(password, saltRounds)
-    const admuser = new AdminUser({name,email,phone, password : hashedPassword});
+    const admuser = new AdminUser({adminname,email,phone, password : hashedPassword});
     await admuser.save()
     res.status(201).json(admuser);
+    console.log("Criado com Sucesso")
   } catch(error : any){
     res.status(400).json({message : error.message})
+    console.log("Falha ao Criar")
   }
 }
 
