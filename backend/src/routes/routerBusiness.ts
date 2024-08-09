a62f0309f6ce700888888a9f19dd814775c05259
dinhoSilwa <dinhosilwa_@hotmail.com>
2024-08-09 03:45:46 -0300

    FIX : remove unused routers

--- backend/src/routes/routerBusiness.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { createBusiness } from "../controllers/admcontrollers";
-
-const routerBusiness = Router();
-routerBusiness.post('/create', createBusiness)
-
-export default routerBusiness
